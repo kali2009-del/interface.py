ddee27914ad2d0b743214dbfddc3cfefc2442eb8
mohammad albustanji <mancyber028@gmail.com>
2025-12-25 12:15:47 -0500

    Add files via upload
    interface.py
    
    interface.py provides a simple command-line interface (CLI) that allows users to easily interact with and execute the network scanning tools included in this project. It acts as a central controller to organize tool execution and improve overall usability.

---
+++ Interface.py
@@ -0,0 +1,10 @@
+from scapy.all import get_if_list
+
+
+interfaces = get_if_list()
+
+print("Available Network Interfaces:")
+print("-----------------------------")
+
+for iface in interfaces:
+    print(iface)
